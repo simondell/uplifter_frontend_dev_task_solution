34d16fdd97e522989d1497bc99f55114b2a8e433
Simon Dell <simon.dell@founders4schools.org.uk>
2020-04-10 11:42:04 +0100

    Scaffold feed data

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useState,
+} from 'react';
+import mockFeed from './__mocks__/example_feed.json'
+
+export interface FeedItem {
+  title: string
+  link: string
+  media: { m: string } 
+  date_taken: string 
+  description: string 
+  published: string
+  author: string 
+  author_id: string
+  tags: string 
+}
 
 function App() {
+  const [feedItems, setFeedItems] = useState([] as FeedItem[])
+
+  useEffect(() => {
+    setFeedItems(mockFeed.items)
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Uplifter Test Solution</h1>
       </header>
       <main>
-        Hello, world
+      {
+        feedItems.map(feedItem =>
+          <div
+            key={`${feedItem.link}`}
+          >
+            {feedItem.media.m}
+          </div>
+        )
+      }
       </main>
     </div>
   );
