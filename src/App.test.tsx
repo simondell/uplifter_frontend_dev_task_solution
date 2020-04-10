4709f711ad5ca67aecb6e8ed8b17effc249e03da
Simon Dell <simon.dell@founders4schools.org.uk>
2020-04-10 10:32:32 +0100

    Strip back App and index to bare basics

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const headingElement = getByText(/Uplifter Test Solution/i);
+
+  expect(headingElement).toBeInTheDocument();
+});
+
+test('renders a main', () => {
+  const { getByText } = render(<App />);
+
+  const mainElement = getByText(/Hello, world/i);
+
+  expect(mainElement).toBeInTheDocument();
 });
