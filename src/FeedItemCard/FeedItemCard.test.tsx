e001e02b05bd8ae5ecbc8e844531b814b4bfb6c4
Simon Dell <simon.dell@founders4schools.org.uk>
2020-04-10 13:56:31 +0100

    Get FeedItemCard to show a photo link and author name

--- src/FeedItemCard/FeedItemCard.test.tsx
+++ src/FeedItemCard/FeedItemCard.test.tsx
@@ -1,26 +1,65 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import FeedItemCard from './FeedItemCard';
+import FeedItemCard, { getAuthorName } from './FeedItemCard';
 import mockFeed from '../App/__mocks__/example_feed.json'
 
 
 test('renders the photo title', () => {
   const mockItem = mockFeed.items[0]
+
   const { getByText } = render(
     <FeedItemCard
       feedItem={mockItem}
     />
   );
 
-  const headingElement = getByText(new RegExp(mockItem.title));
-
-  expect(headingElement).toBeInTheDocument();
+  const itemTitle = getByText(new RegExp(mockItem.title));
+  expect(itemTitle).toBeInTheDocument();
 });
 
-// test('renders a main', () => {
-//   const { getByRole } = render(<FeedItemCard />);
+test('renders a link to the photo\'s page on flickr' , () => {
+  const mockItem = mockFeed.items[0]
 
-//   const mainElement = getByRole(/main/i);
+  const { getByText } = render(
+    <FeedItemCard
+      feedItem={mockItem}
+    />
+  );
 
-//   expect(mainElement).toBeInTheDocument();
-// });
+  const itemTitle = getByText(new RegExp(mockItem.title));
+  expect(itemTitle.closest('a').href).toBe(mockItem.link);
+});
+
+test('renders the author\'s username' , () => {
+  const mockItem = mockFeed.items[0]
+
+  const { getByText } = render(
+    <FeedItemCard
+      feedItem={mockItem}
+    />
+  );
+
+  const author = getByText('haberlerdenizli');
+  expect(author).toBeInTheDocument()
+});
+
+test.skip('renders a link to the author\'s page on flickr' , () => {
+  const mockItem = mockFeed.items[0]
+
+  const { getByText } = render(
+    <FeedItemCard
+      feedItem={mockItem}
+    />
+  );
+
+  const itemTitle = getByText('haberlerdenizli');
+  expect(itemTitle.closest('a').href).toBe(mockItem.link);
+});
+
+describe('getAuthorName()', () => {
+  test('extracts name from author strings', () => {
+    const exampleName = mockFeed.items[0].author // "nobody@flickr.com (\"haberlerdenizli\")"
+
+    expect(getAuthorName(exampleName)).toEqual('haberlerdenizli')
+  })
+})
--- src/FeedItemCard/FeedItemCard.tsx
+++ src/FeedItemCard/FeedItemCard.tsx
@@ -5,12 +5,31 @@ interface FeedItemCardProps {
   feedItem: FeedItem
 }
 
+export function getAuthorName (rawAuthor: string): string | null {
+  const matches = /\("([\w ]+)"\)/.exec(rawAuthor)
+  
+  if(!matches) return matches
+
+  return matches[1]
+}
+
 export default function FeedItemCard (props: FeedItemCardProps) {
   return (
-    <div
-      key={`${props.feedItem.link}`}
+    <article
+      itemScope
+      itemType="https://schema.org/ImageObject"
     >
-      {props.feedItem.title}
-    </div>
+      <header>
+        <h2>
+          <a
+            href={props.feedItem.link}
+          >
+            {props.feedItem.title}
+          </a> by <span>
+            {getAuthorName(props.feedItem.author)}
+          </span>
+        </h2>
+      </header>
+    </article>
   )
 }
